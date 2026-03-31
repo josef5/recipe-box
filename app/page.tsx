38140af94a9d2c5287e58bc5c95f642b52bc0477
Jose Espejo <joseespejo@yahoo.com>
2026-03-31 17:38:33 +0100

    feat: update Home component to display recipes in a list format with links to detail pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { getRecipes } from "@/actions/recipes";
+import Link from "next/link";
 
 export default async function Home() {
   const recipes = await getRecipes();
@@ -6,7 +7,29 @@ export default async function Home() {
   return (
     <main>
       <h1>Recipe Box</h1>
-      <pre>{JSON.stringify(recipes, null, 2)}</pre>
+      {recipes.length === 0 ? (
+        <p>No recipes yet.</p>
+      ) : (
+        <ul>
+          {recipes.map((recipe) => (
+            <li key={recipe.id}>
+              <Link href={`/recipes/${recipe.id}`}>
+                <h2>{recipe.title}</h2>
+                {recipe.description && <p>{recipe.description}</p>}
+                <div>
+                  {recipe.prepTimeMins && (
+                    <span>Prep: {recipe.prepTimeMins}m</span>
+                  )}
+                  {recipe.cookTimeMins && (
+                    <span>Cook: {recipe.cookTimeMins}m</span>
+                  )}
+                  {recipe.servings && <span>Serves: {recipe.servings}</span>}
+                </div>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
     </main>
   );
 }
