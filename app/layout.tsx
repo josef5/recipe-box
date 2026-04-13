a2cafbbcbda0356c396e818efbb89770809b4b6d
Jose Espejo <joseespejo@yahoo.com>
2026-04-13 18:02:46 +0100

    refactor: move menu component into ui folder

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import { Menu } from "@/components/menu";
+import { Menu } from "@/components/ui/menu";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
