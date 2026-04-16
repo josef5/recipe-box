3044b5f488c85d2ac2af3464301d0e69ef991356
Jose Espejo <joseespejo@yahoo.com>
2026-04-16 17:24:04 +0100

    feat: refactor auth middleware to handle redirect logic in proxy function

--- proxy.ts
+++ proxy.ts
@@ -1,8 +1,33 @@
 import { auth } from "@/lib/auth/server";
+import { NextRequest } from "next/server";
 
-export default auth.middleware({
+const authMiddleware = auth.middleware({
   loginUrl: "/auth/sign-in",
 });
+
+export default async function proxy(request: NextRequest) {
+  const response = await authMiddleware(request);
+  const redirectLocation = response.headers.get("location");
+
+  if (!redirectLocation) {
+    return response;
+  }
+
+  const loginUrl = new URL("/auth/sign-in", request.url);
+  const targetUrl = new URL(redirectLocation, request.url);
+
+  if (targetUrl.pathname !== loginUrl.pathname) {
+    return response;
+  }
+
+  if (!targetUrl.searchParams.has("redirectTo")) {
+    const requestedPath = `${request.nextUrl.pathname}${request.nextUrl.search}`;
+    targetUrl.searchParams.set("redirectTo", requestedPath || "/");
+    response.headers.set("location", targetUrl.toString());
+  }
+
+  return response;
+}
 
 export const config = {
   matcher: ["/account/:path*"],
