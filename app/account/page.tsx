afb314a0d84b0c0c44626832fb18ac8b966d626b
Jose Espejo <joseespejo@yahoo.com>
2026-04-17 17:03:56 +0100

    docs: add todo

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -10,6 +10,8 @@ import {
 
 export const dynamic = "force-dynamic";
 
+// TODO: Close change pw accordion on success -> decompose to client component
+
 export default async function AccountPage() {
   const user = await requireCurrentUser({ redirectTo: "/account" });
   const isAdmin = userHasAdminRole(user);
