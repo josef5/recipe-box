d0085c505f70de0d21f5dad7871eb461445199d3
Jose Espejo <joseespejo@yahoo.com>
2026-04-23 16:24:21 +0100

    typescript: add User type definition to types index

---
+++ types/index.ts
@@ -0,0 +1,10 @@
+export type User = {
+  id: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+  email: string;
+  emailVerified?: boolean;
+  name: string;
+  image?: string | null | undefined;
+  role?: string | string[];
+};
