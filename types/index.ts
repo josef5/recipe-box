e0a4481f18a558400ca51bf46bf2e5b97cba0c45
Jose Espejo <joseespejo@yahoo.com>
2026-04-28 00:39:21 +0100

    typescript: define Recipe, Ingredient, RecipeIngredient, and Step types for improved type safety

--- types/index.ts
+++ types/index.ts
@@ -8,3 +8,65 @@ export type User = {
   image?: string | null | undefined;
   role?: string | string[];
 };
+
+export type Recipe = {
+  id: string;
+  userId: string | null;
+  slug: string;
+  title: string;
+  ownerDisplayName?: string | null;
+  description?: string | null;
+  servings?: number | null;
+  prepTimeMins?: number | null;
+  cookTimeMins?: number | null;
+  createdAt: Date;
+  updatedAt: Date;
+  imageUrl?: string | null;
+  imagePublicId?: string | null;
+  sourceUrl?: string | null;
+  sourceName?: string | null;
+  isPublic?: boolean | null;
+  authorId?: string | null;
+  recipeIngredients?: RecipeIngredient[];
+  steps?: Step[];
+};
+
+export type Ingredient = {
+  id: string;
+  amount: string | null;
+  unit: string | null;
+  notes: string | null;
+  ingredient: {
+    name: string;
+  };
+};
+
+export type RecipeIngredient = {
+  id: string;
+  amount: string | null;
+  unit: string | null;
+  notes: string | null;
+  ingredient: {
+    name: string;
+  };
+};
+
+/* export type RecipeIngredient = {
+  id: string;
+  createdAt: Date;
+  updatedAt: Date;
+  recipeId: string;
+  ingredientId: string;
+  amount?: number | null;
+  unit?: string | null;
+  notes?: string | null;
+}; */
+
+export type Step = {
+  id: string;
+  recipeId: string;
+  instruction: string;
+  stepNumber: number;
+  createdAt?: Date;
+  updatedAt?: Date;
+};
