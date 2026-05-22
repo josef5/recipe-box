6f00659c7aedfe81aff7f43d2c10c5fc17d562fd
Jose Espejo <joseespejo@yahoo.com>
2026-05-22 19:15:22 +0100

    style: update spacing in Main component's className for improved layout

--- components/main.tsx
+++ components/main.tsx
@@ -5,7 +5,7 @@ export default function Main({
   return (
     <main
       id="main-content"
-      className="grid grid-cols-1 gap-8 sm:grid-cols-[3fr_1fr]"
+      className="grid grid-cols-1 gap-x-6 gap-y-4 sm:grid-cols-[3fr_1fr]"
       {...props}
     >
       {children}
