49408500c9d3cb08694d4100ff5bddbb6ba96d47
Jose Espejo <joseespejo@yahoo.com>
2026-05-23 12:19:52 +0100

    style: use implicit flex-row for smaller widths

--- components/home-page-content.tsx
+++ components/home-page-content.tsx
@@ -41,7 +41,7 @@ export function HomePageContent({
           noValidate
           className="flex flex-col gap-2"
         >
-          <div className="flex flex-col gap-2 text-sm">
+          <div className="flex gap-2 text-sm sm:flex-col">
             <label htmlFor="recipe-search" className="sr-only">
               Search recipes
             </label>
