c7ea667a8ffd5a1c617e71eb571e64e28c0fea83
Jose Espejo <joseespejo@yahoo.com>
2026-05-31 19:43:35 +0100

    style: apply conditional styling in Accordion and use custom Buttons

--- components/ui/accordion.tsx
+++ components/ui/accordion.tsx
@@ -7,6 +7,7 @@ import React, {
   useState,
 } from "react";
 import { cn } from "@/lib/utils";
+import { Button } from "./button";
 
 export function Accordion({
   heading,
@@ -36,21 +37,45 @@ export function Accordion({
   }));
 
   return (
-    <div className={cn("rounded-lg border p-4", className)} {...props}>
-      <button
-        id={triggerId}
-        type="button"
-        aria-controls={contentId}
-        aria-expanded={isOpen}
-        onClick={() => setIsOpen((prev) => !prev)}
+    <div
+      className={cn(
+        "border-foreground rounded-lg border px-0",
+        isOpen ? "pt-2 pb-4" : "py-0",
+        className,
+      )}
+      {...props}
+    >
+      <div className="flex items-center justify-between">
+        <Button
+          id={triggerId}
+          type="button"
+          variant="ghost"
+          aria-controls={contentId}
+          aria-expanded={isOpen}
+          onClick={() => setIsOpen((prev) => !prev)}
+          className="flex-1 justify-start text-left"
+        >
+          {headingNode ? (
+            headingNode
+          ) : heading ? (
+            <h2 className="text-lg font-semibold">{heading}</h2>
+          ) : null}
+        </Button>
+        <Button
+          type="button"
+          variant="ghost"
+          onClick={() => setIsOpen((prev) => !prev)}
+          className="mr-1 text-xs"
+        >
+          {isOpen ? "⏶" : "⏷"}
+        </Button>
+      </div>
+      <div
+        id={contentId}
+        aria-labelledby={triggerId}
+        hidden={!isOpen}
+        className="px-4"
       >
-        {headingNode ? (
-          headingNode
-        ) : heading ? (
-          <h2 className="text-lg font-semibold">{heading}</h2>
-        ) : null}
-      </button>
-      <div id={contentId} aria-labelledby={triggerId} hidden={!isOpen}>
         {children}
       </div>
     </div>
