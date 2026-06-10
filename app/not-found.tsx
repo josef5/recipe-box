171c002a535eaa2ef93c37d3164ca03df5d77991
Jose Espejo <joseespejo@yahoo.com>
2026-06-10 18:55:48 +0100

    feat: add NotFound component for handling 404 errors

---
+++ app/not-found.tsx
@@ -0,0 +1,16 @@
+import Main from "@/components/main";
+import { BackButton } from "@/components/ui/back-button";
+
+export default function NotFound() {
+  return (
+    <Main>
+      <div className="space-y-4">
+        <h1 className="font-bold">404 - Page Not Found</h1>
+        <p>The page you&apos;re looking for doesn&apos;t exist.</p>
+        <BackButton type="button" variant="primary" fallbackHref="/">
+          Back
+        </BackButton>
+      </div>
+    </Main>
+  );
+}
